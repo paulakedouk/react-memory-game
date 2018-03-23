90f5f2491e343a1390d38b5cfc179a64c750042f
paulakedouk <paulakedouk@gmail.com>
2018-03-22 22:27:22 -0700

    Adding images and files

---
+++ memory-game/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import animals from './animals.json';
+
+class App extends Component {
+  state = {
+    animals
+  };
+
+  score = 0;
+
+  clicked = id => {};
+
+  shuffle = () => {};
+
+  reset = () => {};
+
+  render() {
+    return <div>Testing</div>;
+  }
+}
+
+export default App;
